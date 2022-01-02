30f34f7d9750b7d70cb14a2acea1130b1009ffcd
jiwoo <zbqmgldjfh@gmail.com>
2022-01-02 16:46:15 +0900

    feat(MemberRepository): MemberRepository 구현 완료

--- src/test/java/jpabook/jpashop/MemberRespositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRespositoryTest.java
@@ -12,25 +12,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class MemberRespositoryTest {
 
-    @Autowired
-    MemberRepository memberRespository;
-
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    @DisplayName("Repository 테스트")
-    void testMember() {
-        // given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        // when
-        Long saveId = memberRespository.save(member);
-        Member findMember = memberRespository.find(saveId);
-
-        // then
-        assertThat(member.getId()).isEqualTo(findMember.getId());
-        assertThat(member.getUsername()).isEqualTo(findMember.getUsername());
-        assertThat(findMember).isEqualTo(member);
-    }
 }
