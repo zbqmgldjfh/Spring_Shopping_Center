97b1da5c0e66c5fd9b48fd4ea83c00edbfef9970
jiwoo <zbqmgldjfh@gmail.com>
2022-04-24 15:18:33 +0900

    feat(OrderApiController.ordersV5): DTO를 활용한 collection을 in쿼리로 조회해오기

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -73,6 +73,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDtoOptimization();
+    }
+
     @Getter
     static class OrderDto {
 
